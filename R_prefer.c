8c9e74e476b05a29ba7fe0411cdd1ebb266cd25b
KSB000000 <164139395+KSB000000@users.noreply.github.com>
2025-06-01 14:09:57 +0900

    Add files via upload

---
+++ R_prefer.c
@@ -0,0 +1,73 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define NUM_READERS 5
+#define NUM_WRITERS 2
+
+int readcount = 0;
+sem_t x;
+sem_t wsem;
+
+void* reader(void* arg) {
+    int id = *(int*)arg;
+    while (1) {
+        sem_wait(&x);
+        readcount++;
+        if (readcount == 1)
+            sem_wait(&wsem);
+        sem_post(&x);
+
+        printf("Reader %d is reading...\n", id);
+
+        sem_wait(&x);
+        readcount--;
+        if (readcount == 0)
+            sem_post(&wsem);
+        sem_post(&x);
+
+	}      
+return NULL;
+}
+
+void* writer(void* arg) {
+    int id = *(int*)arg;
+    while (1) {
+        sem_wait(&wsem);
+
+        printf("Writer %d is writing...\n", id);
+
+        sem_post(&wsem);
+
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t rtid[NUM_READERS], wtid[NUM_WRITERS];
+    int ids[NUM_READERS > NUM_WRITERS ? NUM_READERS : NUM_WRITERS];
+
+    sem_init(&x, 0, 1);
+    sem_init(&wsem, 0, 1);
+
+    for (int i = 0; i < NUM_READERS; i++) {
+        ids[i] = i + 1;
+        pthread_create(&rtid[i], NULL, reader, &ids[i]);
+    }
+
+    for (int i = 0; i < NUM_WRITERS; i++) {
+        ids[i] = i + 1;
+        pthread_create(&wtid[i], NULL, writer, &ids[i]);
+    }
+
+    for (int i = 0; i < NUM_READERS; i++)
+        pthread_join(rtid[i], NULL);
+    for (int i = 0; i < NUM_WRITERS; i++)
+        pthread_join(wtid[i], NULL);
+
+    sem_destroy(&x);
+    sem_destroy(&wsem);
+    return 0;
+}
